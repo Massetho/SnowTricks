d52f8ff214258df271cbf2688a4f63f312a38548
Massetho <quentin_thomasset@hotmail.fr>
2018-07-06 15:09:59 +0200

    #9 - Mailer service

---
+++ src/Service/Mailer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @description :
+ * @package : PhpStorm.
+ * @Author : quent
+ * @date: 18/05/2018
+ * @time: 15:38
+ */
+
+namespace App\Service;
+
+use \SendGrid;
+
+class Mailer
+{
+    /**
+     * @var string $sendgridKey
+     */
+    private $sendgridKey;
+
+    /**
+     * @var string $adminMail
+     */
+    private $adminMail;
+
+    //FUNCTIONS
+
+    /**
+     * @param string $sendgridKey
+     * @param string $adminMail
+     */
+    public function __construct($sendgridKey, $adminMail)
+    {
+        $this->sendgridKey = $sendgridKey;
+        $this->adminMail = $adminMail;
+    }
+
+    //GETTERS & SETTERS
+
+    /**
+     * @param string $subject
+     * @param string $content
+     * @param string $targetName
+     * @param string $targetMail
+     */
+    public function sendMail($subject,
+                             $content,
+                             $targetName,
+                             $targetMail)
+    {
+        $from = new SendGrid\Email("Blogpro", $this->adminMail);
+        $to = $targetName;
+        $to = new SendGrid\Email($to, $targetMail);
+
+        $content = new SendGrid\Content("text/html", $content);
+        $mail = new SendGrid\Mail($from, $subject, $to, $content);
+
+        $sg = new \SendGrid($this->sendgridKey);
+        $sg->client->mail()->send()->post($mail);
+    }
+}
